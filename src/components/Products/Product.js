6378ba0f0515bd6c59ded07a32220c008679ffd7
brian biomdo <bkbiomdo@gmail.com>
2023-01-28 10:59:45 +0300

    Data Grid for Products

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -26,10 +26,19 @@ const columns = [
     // editable: true,
   },
   {
+    field: 'clients',
+    headerName: 'Onboarded clients',
+    type: 'number',
+    width: 150,
+    // editable: true,
+  },
+  {
     field: 'amount',
     headerName: 'Amount',
-    type: 'number',
-    width: 110,
+    // type: 'number',
+    width: 150,
+    align: 'right',
+    headerAlign: 'right',
     // editable: true,
   },
   //   {
@@ -51,17 +60,40 @@ function Product() {
       // console.log(error)
     })
     if (res && res.data) {
-      res.data.map((prod) => {
-        prod.duration = parseDays(prod.period)
-      })
       setProducts(res.data)
     }
   }
 
+  const setProductRows = () => {
+    products.map((product) => {
+      axios
+        .get(`/product/clientproducts/${product.id}`)
+        .then((res) => {
+          product.duration = parseDays(product.period)
+          product.clients = res.data.length
+          product.amount = formatter.format(product.amount)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+    setRows(products)
+  }
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'KES',
+
+    // Options are needed to round to whole numbers
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  })
+
   useEffect(() => {
     fetchProducts()
-    setRows(products)
-  }, [products])
+    setProductRows()
+  }, [products.length])
+
   return (
     <Box sx={{ height: '650px', width: '100%' }}>
       <DataGrid
